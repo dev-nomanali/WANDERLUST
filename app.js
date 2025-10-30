19c204ab3248964c8ba7996cf96f0e0217cec7a7
dev-nomanali <devnomanali@gmail.com>
2025-10-30 13:46:52 +0530

    create review form and add some validations

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -44,12 +45,22 @@ const validateListing = (req, res, next) => {
     }
     const { error } = listingSchema.validate(req.body);
     if (error) {
-      let msg = error.details.map((el) => el.message).join(", ");
-      throw new ExpressError(400, msg);
+      let errMsg = error.details.map((el) => el.message).join(", ");
+      throw new ExpressError(400, errMsg);
     }else{
     next();
     }
-}
+};
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+      let errMsg = error.details.map((el) => el.message).join(", ");
+      throw new ExpressError(400, errMsg);
+    }else{
+    next();
+    }
+};
 
 //index route
 app.get("/listings",
@@ -109,6 +120,29 @@ app.delete("/listings/:id",
         res.redirect("/listings");
     })
 );
+
+//Reviews
+//post Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
+    try {
+        let listing = await Listing.findById(req.params.id);
+        console.log(req.body);
+    let newReview = new Review(req.body.review);
+
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}));
 
 // app.get("/testlisting", async (req, res) =>{
 //     let sampleListing = new Listing({
--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 const joi = require("joi");
+const review = require("./models/review");
 
 module.exports.listingSchema = joi.object({
     listing : joi.object({
@@ -12,4 +13,11 @@ module.exports.listingSchema = joi.object({
       filename: joi.string().allow("", null)
     }).optional()
   }).required()
+});
+
+module.exports.reviewSchema = joi.object({
+  review: joi.object({
+    rating: joi.number().required().min(1).max(5),
+    comment: joi.string().required(),
+  }).required(),
 });

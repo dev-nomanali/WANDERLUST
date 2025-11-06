9b1db142db20df8ae21d1d5d9f67503081abe493
dev-nomanali <devnomanali@gmail.com>
2025-11-06 20:10:43 +0530

    add alerts for flash

--- routes/listing.js
+++ routes/listing.js
@@ -43,6 +43,10 @@ router.get("/:id",
     wrapAsync(async (req, res) => {
         let { id } = req.params;
         const listing = await Listing.findById(id).populate("reviews");
+        if(!listing) {
+            req.flash("error", "Listing you requested for does not exist!");
+            return res.redirect("/listings");
+        }
         res.render("listings/show.ejs", { listing });
     })
 );
@@ -62,6 +66,10 @@ router.get("/:id/edit",
     wrapAsync(async (req, res) => {
         let { id } = req.params;
         const listing = await Listing.findById(id);
+        if(!listing) {
+            req.flash("error", "Listing you requested for does not exist!");
+            return res.redirect("/listings");
+        }
         res.render("listings/edit.ejs", { listing });
     })
 );
@@ -71,6 +79,7 @@ router.put("/:id",
     wrapAsync(async (req, res) => {
         let { id } = req.params;
         await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+         req.flash("success", "Listing Updated!");
         res.redirect(`/listings/${id}`);
     })
 );
@@ -80,6 +89,7 @@ router.delete("/:id",
         let { id } = req.params;
         let deletedListing = await Listing.findByIdAndDelete(id);
         console.log(deletedListing);
+        req.flash("success", "Listing Deleted!");
         res.redirect("/listings");
     })
 );

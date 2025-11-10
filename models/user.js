6cac5d7a365afc812e005baf8e8c1620cea8eec0
dev-nomanali <devnomanali@gmail.com>
2025-11-10 19:59:05 +0530

    add new schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 
 const reviewSchema = new Schema({
@@ -16,6 +23,18 @@ const reviewSchema = new Schema({
 });
 
 const revieSchema = new Schema({
+    comment: String,
+    rating: {
+        type:Number,
+        main: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+const reviSchema = new Schema({
     comment: String,
     rating: {
         type:Number,
